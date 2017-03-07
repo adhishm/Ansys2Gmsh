7dfe81533fb3bc1fd413bd0684cea822343e3f35
Adhish Majumdar <adhish.majumdar@geonx.com>
2017-03-07 20:36:57 +0100

    Parsing Ansys mesh file: In progress

--- Gmsh/GmshMesh.cs
+++ Gmsh/GmshMesh.cs
@@ -7,6 +7,10 @@ using System.IO;
 
 namespace Gmsh
 {
+    public enum ImportFromType
+    {
+        ANSYS
+    }
     public class GmshMesh
     {
         #region Private Fields
@@ -17,6 +21,7 @@ namespace Gmsh
         #region Public Properties
         public List<GmshMeshNode> Nodes { get { return _nodes; } }
         public List<GmshMeshElement> Elements { get { return _elements; } }
+        public System.Globalization.CultureInfo Format = new System.Globalization.CultureInfo("en-US");
         #endregion
 
         #region Constructor
@@ -24,6 +29,18 @@ namespace Gmsh
         {
             _nodes = nodes;
             _elements = elements;
+        }
+
+        public GmshMesh(string filename, ImportFromType importType = ImportFromType.ANSYS)
+        {
+            switch(importType)
+            {
+                case ImportFromType.ANSYS:
+                    _parseAnsysMeshFile(filename);
+                    break;
+                default:
+                    break;
+            }
         }
         #endregion
 
@@ -46,6 +63,41 @@ namespace Gmsh
             lines.Add("");
 
             File.WriteAllLines(filename, lines);
+        }
+        #endregion
+
+        #region Private methods
+        private void _parseAnsysMeshFile(string filename)
+        {
+            string[] lines = File.ReadAllLines(filename);
+
+            _parseAnsysMeshNodes(lines);
+        }
+
+        private void _parseAnsysMeshNodes(string[] lines)
+        {
+            int lineCount = 0;
+            string firstWord = "";
+            while (firstWord != "NBLOCK")
+            {
+                string[] words = lines[lineCount++].Split(',');
+                firstWord = words.First();
+            }
+
+            // Now we are in the zone of the node definitions
+            string[] items = lines[lineCount++].Split('(', ',', ')', 'i', 'e');
+            int numInts = int.Parse(items[0], Format);
+            int sizeInts = int.Parse(items[1], Format);
+            int numFloats = int.Parse(items[2], Format);
+            float floatSize = float.Parse(items[3], Format);
+
+            int nodeCount = 0;
+            string line = lines[lineCount++];
+            firstWord = line.Split(',').First();
+            while (firstWord != "N")
+            {
+                
+            }
         }
         #endregion
     }
