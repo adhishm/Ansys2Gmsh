1b24d9217432eeabcd05023a2b0b5c20c72262ad
Adhish Majumdar <adhish.majumdar@geonx.com>
2017-03-07 17:37:48 +0100

    Add project files.

---
+++ Ansys2Gmsh/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ansys2Gmsh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
